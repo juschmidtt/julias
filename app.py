d0314084d77ad122142182995584bb1e1ef0143d
juschmidtt <juschmidt1302@gmail.com>
2025-04-29 11:24:11 -0300

    correção da aplicação

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, request
-import uuid
-import psycopg
+import requests
+
+url = "https://www.omdbapi.com/?apikey=c45ea72&"
+
 
 app = Flask(__name__)
-connection_db = psycopg.connect("dbname=julias user=postgres password=3f@db host=164.90.152.205 port=80")
+
+@app.route("/titulo", methods=["GET"])
+def titulo():
+    dados = request.json()
+    titulo2 = dados["titulo"]
+    requests.get(url + "t=" + titulo2)
+
+
+
+@app.route("/id", methods=["GET"])
+def id1():
+    dados = request.json()
+    id2 = dados["id"]
+    requests.get(url + "i=" + id2)
