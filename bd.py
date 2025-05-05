edd9cf3c3c8b012c8380d199ebed7b1cc4bc509b
juschmidtt <141142651+juschmidtt@users.noreply.github.com>
2025-05-05 12:06:40 -0300

    inserção do bd

--- bd.py
+++ bd.py
@@ -1,5 +1,46 @@
+import os
 import psycopg
 
-conn = psycopg.connect("dbname=julias user=postgres password=3f@db host=164.90.152.205 port=80")
+import json
 
-cur = conn.cursor() 
+
+
+from config import URLBD# type: ignore
+
+conexao = psycopg.connect(URLBD)
+conexao.execute("""
+    CREATE TABLE IF NOT EXISTS filmes (
+        id SERIAL PRIMARY KEY,
+        imdb_id TEXT UNIQUE,
+        titulo TEXT,
+        ano TEXT,
+        tipo TEXT,
+        data JSONB
+    );
+""")
+conexao.commit()
+
+def encontrarfilme_titulo(titulo):
+    with conexao.cursor() as cur:
+        cur.execute("SELECT * FROM filmes WHERE titulo = %s", (titulo,))
+        return cur.fetchone()
+
+def encontrarfilme_id(imdb_id):
+    with conexao.cursor() as cur:
+        cur.execute("SELECT * FROM filmes WHERE id_imdb = %s", (imdb_id,))
+        return cur.fetchone()
+
+
+def salvarfilme(filme):
+    with conexao.cursor() as cur:
+        cur.execute("""
+            INSERT INTO filmes (id_imdb, titulo, ano, tipo, data)
+            VALUES (%s, %s, %s, %s, %s)
+            ON CONFLICT (id_imdb) DO NOTHING
+        """, (
+            filme.get("imdbID"),
+            filme.get("Title"),
+            filme.get("Year"),
+            filme.get("Type"),
+            json.dumps(filme)
+        ))
